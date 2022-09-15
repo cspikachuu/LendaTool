169e989a2acf9c701f80cc834f3edbd466c8c5d0
bprhee01 <73865293+bprhee01@users.noreply.github.com>
2022-09-14 22:16:46 -0400

    Finished Login & Signup, waiting for endpoints

--- client/pages/SignUp.tsx
+++ client/pages/SignUp.tsx
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { useParams, useNavigate } from 'react-router-dom';
+
 import {
   FormControl,
   FormLabel,
@@ -9,23 +11,34 @@ import {
 } from '@chakra-ui/react'
 
 export default function SignUp() {
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const navigate = useNavigate();
+
+  async function signup(){
+    //add route
+    navigate("/login", { replace: true })
+  }
+
+
+
   return (
     <>
       <div id="signup-container">
         <h1> Signup </h1>
         <FormControl isRequired>
           <FormLabel>First Name</FormLabel>
-          <Input id="first-name" type="text"/>
+          <Input id="first-name" type="text" onChange={e => { setFirstName(e.target.value) }}/>
           <FormLabel>Last Name</FormLabel>
-          <Input id="last-name" type="text"/>
-          <FormLabel>Email</FormLabel>
-          <Input id="email" type="email"/>
+          <Input id="last-name" type="text" onChange={e => { setLastName(e.target.value) }}/>
           <FormLabel>Username</FormLabel>
-          <Input id="username" type="text"/>
+          <Input id="username" type="text" onChange={e => { setUsername(e.target.value) }}/>
           <FormLabel>Password</FormLabel>
-          <Input id="password" type="text"/>
+          <Input id="password" type="text" onChange={e => { setPassword(e.target.value) }}/>
         </FormControl>
-        <Button style={{margin: '25px 0px', padding: 'auto'}} colorScheme="orange">Submit</Button>
+        <Button style={{margin: '25px 0px', padding: 'auto'}} colorScheme="orange" onClick={(event)=> signup()}>Submit</Button>
       </div>
     </>
   )
