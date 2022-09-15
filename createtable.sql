e96ee33523be428be22131ed946c3ba82c46de96
thelucidcoder <thelucidcoder@protonmail.org>
2022-09-15 00:56:59 -0400

    Database created and queried

---
+++ createtable.sql
@@ -0,0 +1,39 @@
+CREATE TABLE  users (
+	"user_id" int NOT NULL,
+	"firstname" char NOT NULL,
+	"lastname" char NOT NULL,
+	"career" char,
+  "tool_id" int,
+	PRIMARY KEY ("user_id")
+);
+
+CREATE TABLE  tools (
+	"tool_id" int NOT NULL,
+	"listingName" char NOT NULL,
+  "photo" char,
+	"condition" char NOT NULL,
+	"type" char NOT NULL,
+	"price" char NOT NULL,
+	"status" boolean NOT NULL,
+	"dateCreated" TIMESTAMP NOT NULL,
+  "user_id" int NOT NULL,
+	PRIMARY KEY ("tool_id"), 
+  CONSTRAINT "fk_user" FOREIGN KEY("user_id")
+	  REFERENCES users ("user_id")
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE  borrower (
+	"borrower_id" int NOT NULL,
+	"user" char NOT NULL,
+	"tool_id" int NOT NULL,
+  "user_id" int NOT NULL,
+  PRIMARY KEY ("borrower_id"),
+	CONSTRAINT "fk_b_tool" FOREIGN KEY("tool_id")
+	  REFERENCES tools ("tool_id")
+    ON DELETE SET NULL,
+  CONSTRAINT "fk_b_user" FOREIGN KEY("user_id")
+	  REFERENCES users ("user_id")
+    ON DELETE SET NULL
+);
--- server/routes/tool.ts
+++ server/routes/tool.ts
@@ -2,12 +2,54 @@
 const express = require('express');
 const toolController = require('../controllers/toolController');
 const toolRouter = express.Router();
+const db = require('../models/apiModel');
 import { Request, Response, NextFunction, RequestHandler, ErrorRequestHandler, Application} from "express";
 
 
-toolRouter.post('/',
-  
-)
+toolRouter.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  const { tool_id, listingName, photo, condition, type, price, status} = req.body;
+  try {
+    //add query
+    await db.query()
+    // return them to a completed page
+    return res.status(200).json('tools added')
+  }
+  //appropriately add global error handler
+  catch (err) {
+    console.log(err)
+    return res.status(404).json('tools could not be added')
+  }
 
+})
 
+toolRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
+
+  try {
+ await db.query('SELECT * FROM tools')
+
+    return res.status(200).json('tools retrieved')
+  }
+  //appropriately add global error handler
+  catch (err) {
+    console.log(err)
+    return res.status(404).json('tools could not be retrieved')
+  }
+
+})
+
+toolRouter.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+
+  try {
+    req.params
+      //find a tool by anything passed into the params from tools route
+     await db.query()
+
+    return res.status(200).json('tools retrieved')
+  }
+  //appropriately add global error handler
+  catch (err) {
+    console.log(err)
+    return res.status(404).json('tools could not be added')
+  }
+})
 module.exports = toolRouter; 
