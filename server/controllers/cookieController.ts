6b7d2caa7b6e9e9f0e1d4d97a0989325564d8a53
Rosendo De Luna <rosendo.isra.deluna@gmail.com>
2022-09-15 19:13:37 -0600

    added comments

--- server/controllers/cookieController.ts
+++ server/controllers/cookieController.ts
@@ -1,21 +1,20 @@
 //Imports here
 import { Request, Response, NextFunction, RequestHandler, ErrorRequestHandler, Application} from "express";
 
+/* This is creating a cookieController object with two methods, setCookie and setSSIDCookie. */
 const cookieController = {
+  /* sets a cookie for the user */
   setCookie: async (req: Request, res: Response, next: NextFunction) => {
-    console.log("in setCookie");
-    //what do I put in cookie??
     const userInfo = res.locals.user.username;
     res.cookie('username', userInfo);
     return next();
-      
     },
 
   setSSIDCookie:  async (req: Request, res: Response, next: NextFunction) => {
     res.cookie('ssid', res.locals.newUser, { httpOnly: true });
-    console.log("in setSSIDCookie");
     return next();
     }
 };
 
+/* Exporting the cookieController object so that it can be imported into other files. */
 module.exports = cookieController;
--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,63 +1,45 @@
-//Imports and requirements here
 const db = require('../models/sqlModel');
-
-const bcrypt = require('bcrypt'); //is it still bcrypt with "js" or change to "ts" ??
+const bcrypt = require('bcrypt');
 import { Request, Response, NextFunction, RequestHandler, ErrorRequestHandler, Application} from "express";
 
 
+/*  user controller object with two methods */
 const userController = {
 
-  getAllUsers: async (req: Request, res: Response, next: NextFunction) => {
-    const getQuery = 'SELECT * FROM Users';
-    try {
-      const data = await db.query(getQuery);
-      res.locals.users = data.rows;
-      return next();
-    }
-    catch (err) {
-      return next(err);
-    }
-    
-    },
-
+/*  createUser method is destructuring the username, password, firstname, and lastname from the request body. It
+then hashes the password using bcrypt and inserts the user into the database. */
   createUser: async (req: Request, res: Response, next: NextFunction) => {
-
-    
     try {
-      
       const {username, password, firstname, lastname} = req.body
       const hashedPass = await bcrypt.hash(password, 10);
-      const query= "INSERT INTO users (username, password, firstname, lastname) VALUES ($1, $2, $3, $4) RETURNING user_id, username"
-
+      const query = "INSERT INTO users (username, password, firstname, lastname) VALUES ($1, $2, $3, $4) RETURNING user_id, username";
       const newUserQuery = await db.query(query, [username, hashedPass, firstname, lastname]);
-      
-
       res.locals.registered = true
       return next();
     }
     catch (err) {
-      console.log(err)
       return next({err: "Username taken try another"})
     }
     },
 
+  /* verify user middleware */
   verifyUser:  async (req: Request, res: Response, next: NextFunction) => {
-    console.log("in verifyUser");
     try {
+      /* Destructuring the username and password from the request body. */
       const { username, password } = req.body;
-      const existingUser = await /* pool.query or --> */ db.query(
+      /* Querying the database for a user with the username that was passed in the request body. */
+      const existingUser = await db.query(
         "SELECT * from users WHERE username=$1",
         [username]
       );
-      // if (theUser.length === 0 || !bcrypt.compareSync(password, theUser[0].password))
+      /* Checking if the user exists in the database or if the password is correct. */
       if (existingUser.rows.length === 0 || !bcrypt.compareSync(password, existingUser.rows[0].password)) {
         res.locals.user = false;
         return res.status(200).json(res.locals.user)
       } else {
-        res.locals.user = existingUser.rows[0]; //existingUser.rows ????
+        res.locals.user = existingUser.rows[0]; 
         return next();
       }
-
     }
     catch (err) {
       return next({ err: "error verifying user" });
